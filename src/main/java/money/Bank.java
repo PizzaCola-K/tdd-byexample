803b772b7384095af808b1df5acff530447a8ac8
K <kihyuk.sung@outlook.kr>
2021-06-28 19:10:07 +0900

    refactor: Now Bank has responsibility to show rate
    - 이제 은행이 환전 비율을 계산해주는 책임을 갖습니다.

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -3,9 +3,13 @@ package money;
 public class Bank {
 
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     public void addRate(String chf, String usd, int i) {
+    }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 }
