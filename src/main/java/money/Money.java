0563996d5f464bfe6ff2f6c272ae4f1ecefcd45b
K <kihyuk.sung@outlook.kr>
2021-06-28 17:40:04 +0900

    feat: Money.currency
    - 이제 돈은 currency()를 부르면, 통화코드를 리턴한다.

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -3,6 +3,9 @@ package money;
 public abstract class Money {
     protected int amount;
 
+    public abstract Money times(int multiplier);
+    public abstract String currency();
+
     public static Money dollar(int amount) {
         return new Dollar(amount);
     }
@@ -10,8 +13,6 @@ public abstract class Money {
     public static Money franc(int amount) {
         return new Franc(amount);
     }
-
-    public abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object object) {
