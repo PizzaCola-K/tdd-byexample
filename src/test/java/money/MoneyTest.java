9de1539b328158a249c59a5fdd1c2a42d32b0a61
K <kihyuk.sung@outlook.kr>
2021-06-28 18:12:48 +0900

    test: Remove useless test code
    - 객체를 통합해서 필요없어진 테스트 코드를 삭제했다.

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -14,18 +14,9 @@ class MoneyTest {
     }
 
     @Test
-    void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertThat(five.times(2)).isEqualTo(Money.franc(10));
-        assertThat(five.times(3)).isEqualTo(Money.franc(15));
-    }
-
-    @Test
     void testEquality() {
         assertThat(Money.dollar(5)).isEqualTo(Money.dollar(5));
         assertThat(Money.dollar(5)).isNotEqualTo(Money.dollar(6));
-        assertThat(Money.franc(5)).isEqualTo(Money.franc(5));
-        assertThat(Money.franc(5)).isNotEqualTo(Money.franc(6));
         assertThat(Money.franc(5)).isNotEqualTo(Money.dollar(5));
     }
 
