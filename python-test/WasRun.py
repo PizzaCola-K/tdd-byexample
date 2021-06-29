69af33924923fecc53dde7b92ff8efd7d1d56f40
K <kihyuk.sung@outlook.kr>
2021-06-29 17:36:27 +0900

    feat: Can run method by attribute
    - name을 받아서, WasRun 메소드 내의 메소드 실행

--- python-test/WasRun.py
+++ python-test/WasRun.py
@@ -4,6 +4,12 @@ from this import d
 class WasRun:
     def __init__(self, name) -> None:
         self.wasRun = None
+        self.name = name
+
+    def run(self) -> None:
+        method = getattr(self, self.name)
+        method()
 
     def testMethod(self) -> None:
         self.wasRun = 1
+        
