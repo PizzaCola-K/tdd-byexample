7e50829afe1f867800bde21a6d52e928062931df
K <kihyuk.sung@outlook.kr>
2021-06-29 18:20:56 +0900

    feat: Test SetUp
    - setUp()을 테스트하고 구현했다.
    - 예제 테스트 케이스를 단순화 하기 위해 setUp()을 사용했다.
    - 예제 테스트 케이스에 대한 테스트 케이스를 단순화 하기 위해
      setUp()을 사용했다.

--- python-test/TestCaseTest.py
+++ python-test/TestCaseTest.py
@@ -3,18 +3,16 @@ from TestCase import TestCase
 
 
 class TestCaseTest(TestCase):
-    def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
-    
-    def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasSetUp)
 
     def setUp(self):
-        pass
+        self.test = WasRun('testMethod')
+
+    def testRunning(self):
+        self.test.run()
+        assert(self.test.wasRun)
+    
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testSetUp").run()
--- python-test/WasRun.py
+++ python-test/WasRun.py
@@ -4,11 +4,11 @@ from this import d
 
 class WasRun(TestCase):
     def __init__(self, name) -> None:
-        self.wasRun = None
         TestCase.__init__(self, name)
 
     def testMethod(self) -> None:
         self.wasRun = 1
 
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
