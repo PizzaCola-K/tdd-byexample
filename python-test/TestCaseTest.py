440a3f2fd2e6cddc4cc420be92d3d24b32485bac
K <kihyuk.sung@outlook.kr>
2021-06-29 20:38:57 +0900

    feat: WasRun에 Log 남기기

--- python-test/TestCaseTest.py
+++ python-test/TestCaseTest.py
@@ -5,14 +5,11 @@ from TestCase import TestCase
 class TestCaseTest(TestCase):
 
     def setUp(self):
-        self.test = WasRun('testMethod')
+        pass
+ 
+    def testTemplateMethod(self):
+        test = WasRun('testMethod')
+        test.run()
+        assert('setUp testMethod ' == test.log)
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-    
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
-
-TestCaseTest("testSetUp").run()
+TestCaseTest('testTemplateMethod').run()
