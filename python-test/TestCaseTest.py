83ff47487c8f5a012233f81896aa57e8b0b25f21
K <kihyuk.sung@outlook.kr>
2021-07-02 21:29:09 +0900

    feat: TestCaseTest

--- python-test/TestCaseTest.py
+++ python-test/TestCaseTest.py
@@ -5,33 +5,34 @@ from TestSuite import TestSuite
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
+
     def testTemplateMethod(self):
         test = WasRun('testMethod')
-        test.run()
+        test.run(self.result)
         assert('setUp testMethod tearDown ' == test.log)
 
     def testResult(self):
         test = WasRun('testMethod')
-        result = test.run()
+        test.run(self.result)
         assert('1 run, 0 failed' == result.summary())
     
     def testFailedResult(self):
         test = WasRun('testMethod')
-        result = test.run()
+        test.run(self.result)
         assert('1 run, 1 failed' == result.summary())
 
     def testFailedResultFormatting(self):
-        result = TestResult()
-        result.testStarted()
-        result.testFailed()
+        self.result.testStarted()
+        self.result.testFailed()
         assert('1 run, 1 failed' == result.summary())
     
     def testSuite(self):
         suite = TestSuite()
         suite.add(WasRun('testMethod'))
         suite.add(WasRun('testBrokenMethod'))
-        result = TestResult()
-        suite.run(result)
+        suite.run(self.result)
         assert('2 run, 1 failed' == result.summary())
         
 suite = TestSuite()
